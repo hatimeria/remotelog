f427451ab97c84bd56998e1cdf6342289491ac47
freakphp <me@freakphp.com>
2012-01-18 16:08:41 +0100

    Initial commit

---
+++ src/Remotelog/Logger.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Remotelog;
+
+class Logger
+{
+    /**
+     * Host server
+     * @var string
+     */
+    protected $server;
+    /**
+     * Route pattern
+     *
+     * @var string
+     */
+    protected $route;
+    /**
+     * Place string
+     *
+     * @var string
+     */
+    protected $place;
+
+    public function __construct($server, $place, $route = '/api/monitoring')
+    {
+        $this->server = $server;
+        $this->place  = $place;
+        $this->route  = $route;
+    }
+
+    public function addLog($log)
+    {
+        if ($log instanceof \Exception) {
+            /* @var \Exception $e */
+            $e = $log;
+
+            $log = array(
+                'message'     => $e->getMessage(),
+                'stack_trace' => $e->getTrace(),
+                'type'        => 'exception',
+                'place'       => $this->place
+            );
+        } elseif (is_array($log)) {
+            $log = array_merge(array(
+                'place' => $this->place,
+            ),$log);
+        } else {
+            return;
+        }
+
+        $post = json_encode($log);
+
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL, $this->server . $this->route);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8'));
+        curl_setopt($ch,CURLOPT_POST,true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$post);
+
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+    }
+
+}
