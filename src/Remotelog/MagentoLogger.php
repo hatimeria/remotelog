f0d1bed5e54c66b3c5c30e42b23309e8ce55ee23
freakphp <me@freakphp.com>
2012-03-15 11:46:48 +0100

    Magento logger  additional attributes

--- src/Remotelog/MagentoLogger.php
+++ src/Remotelog/MagentoLogger.php
@@ -2,17 +2,25 @@
 
 namespace Remotelog;
 
+use Mage;
+
 class MagentoLogger extends Logger
 {
     public function addLog($log)
     {
+        if (true !== Mage::registry('remotelog_enabled')) {
+            return;
+        }
         $trace = $log[1];
         $trace = explode("\n", $trace);
 
         $mageLog = array(
             'message'     => $log[0],
             'stack_trace' => $trace,
-            'type'        => 'critical'
+            'type'        => 'CRITICAl',
+            'code'        => 500,
+            'url'         => Mage::helper('core/http')->getRequestUri(),
+            'client_ip'   => Mage::helper('core/http')->getRemoteAddr(),
         );
 
         parent::addLog($mageLog);
